164f8ef2242e40fd23d3e16e1c23aecd98bf5246
T-5150 <timucinsur@gmail.com>
2024-05-25 13:06:41 +0300

    feat: create image model

--- core/models.py
+++ core/models.py
@@ -33,9 +33,47 @@ def __str__(self):
     return f'General Setting: {self.name}'
 
 
-
-
 class Meta:
-    vebose_name = 'General Setting'
-    vebose_name_plural = 'General Settings'
+    verbose_name = 'General Setting'
+    verbose_name_plural = 'General Settings'
     ordering = ('name',)
+
+
+class ImageSetting(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Name',
+    )
+    description = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Description',
+    )
+    file = models.ImageField(
+        default='',
+        verbose_name='Image',
+        help_text='',
+        blank=True,
+        upload_to='images/',
+
+    )
+    updated_date = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+    )
+    created_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f'Image Setting: {self.name}'
+
+    class Meta:
+        verbose_name = 'Image Setting'
+        verbose_name_plural = 'Image Settings'
+        ordering = ('name',)
+
